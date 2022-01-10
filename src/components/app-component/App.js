7f049c71cd1a6da471e912907c79ff17d7f2bb60
ElenaZht <elenazht@gmail.com>
2022-01-10 22:46:05 +0200

    - Initial commit. - App component created.

---
+++ src/components/app-component/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './app.css';
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <p className="signature">Elena Zhytomirskaya, 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
